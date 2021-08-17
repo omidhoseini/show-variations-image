b2abe77bdca548ed66a72c51f5d94d67c95ed0b6
omidhoseini <webbmakerr@gmail.com>
2021-08-17 21:38:54 +0430

    Add if-statement for checking existence of "zoomImg" class & "data-product_variations" attribute.
    [Ticket: 20210817#1.1.2]

--- inc/js/show-var-img-on-single-attr.js
+++ inc/js/show-var-img-on-single-attr.js
@@ -1,20 +1,19 @@
 /* jshint esversion:6 */
 jQuery(function ($) {
-
     $(window).bind('load', function () {
-
+        if (!$('img').hasClass('zoomImg')) {
+            return;
+        }
+        if (!$('.variations_form').attr('data-product_variations')) {
+            return;
+        }
         let base_image = $('.zoomImg')[0].src;
         let variations = JSON.parse($('.variations_form').attr('data-product_variations'));
-
         if (variations) {
-
             let first_attr = Object.keys(variations[0].attributes)[0];
-
             $('select[data-attribute_name="' + first_attr + '"]').change(function () {
-
                 let found = false;
                 let value_of_choice = $(this).val();
-
                 let image_to_show = '';
                 let main_base_image = '';
                 $('.zoomImg').removeAttr('src');
@@ -22,15 +21,11 @@ jQuery(function ($) {
                 $('.wp-post-image').removeAttr('srcset');
                 $('.wp-post-image').removeAttr('data-src');
                 $('.wp-post-image').removeAttr('data-large_image');
-
                 for (const i in variations) {
-
                     if (found) {
                         continue;
                     }
-
                     if (value_of_choice) {
-
                         if (variations.hasOwnProperty(i)) {
                             if (value_of_choice === variations[i].attributes[Object.keys(variations[0].attributes)[0]]) {
                                 image_to_show = variations[i].image.url;
@@ -39,29 +34,23 @@ jQuery(function ($) {
                             }
                         }
                     } else {
-
                         main_base_image = base_image;
                         found = true;
                         console.log('main_base_image_src: ' + main_base_image);
                     }
                 }
-
                 if (image_to_show.length) {
-
                     $('.zoomImg').attr('src', image_to_show).removeAttr('srcset');
                     $('.wp-post-image').attr('src', image_to_show).removeAttr('srcset');
                     $('.wp-post-image').attr('data-src', image_to_show).removeAttr('srcset');
                     $('.wp-post-image').attr('data-large_image', image_to_show).removeAttr('srcset');
                 }
-
                 if (main_base_image.length) {
-
                     $('.zoomImg').attr('src', main_base_image).removeAttr('srcset');
                     $('.wp-post-image').attr('src', main_base_image).removeAttr('srcset');
                     $('.wp-post-image').attr('data-src', main_base_image).removeAttr('srcset');
                     $('.wp-post-image').attr('data-large_image', main_base_image).removeAttr('srcset');
                 }
-
             });
         }
     });
